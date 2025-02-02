4fcf462ff47e24529877c37e15edc2ed3742a396
Zhuanz密码0000 <Zhuanz@192.168.1.5>
2025-02-02 21:53:45 +0800

    [250202] reformat

--- coding/src/main/java/org/person/interview/dongtaiguihua/CJMax_152.java
+++ coding/src/main/java/org/person/interview/dongtaiguihua/CJMax_152.java
@@ -2,27 +2,27 @@ package org.person.interview.dongtaiguihua;
 
 public class CJMax_152 {
 
-    public static int maxProduct(int[] nums) {
-        long maxF = nums[0];
-        long minF = nums[0];
-        int ans = nums[0];
-        int length = nums.length;
-        for (int i = 1; i < length; i++) {
-            long mx = maxF;
-            long mn = minF;
-            maxF = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
-            minF = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
-            if (minF<-1<<31) {
-                minF = nums[i];
-            }
-            ans = Math.max((int)maxF, ans);
-        }
-        return ans;
+  public static int maxProduct(int[] nums) {
+    long maxF = nums[0];
+    long minF = nums[0];
+    int ans = nums[0];
+    int length = nums.length;
+    for (int i = 1; i < length; i++) {
+      long mx = maxF;
+      long mn = minF;
+      maxF = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
+      minF = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
+      if (minF < -1 << 31) {
+        minF = nums[i];
+      }
+      ans = Math.max((int) maxF, ans);
     }
+    return ans;
+  }
 
-    public static void main(String[] args) {
-        int[] nums = {2,3,-2,4};
-        int res = maxProduct(nums);
-        System.out.println(res);
-    }
+  public static void main(String[] args) {
+    int[] nums = {2, 3, -2, 4};
+    int res = maxProduct(nums);
+    System.out.println(res);
+  }
 }
--- coding/src/main/java/org/person/interview/dongtaiguihua/ZcKh_32.java
+++ coding/src/main/java/org/person/interview/dongtaiguihua/ZcKh_32.java
@@ -2,25 +2,25 @@ package org.person.interview.dongtaiguihua;
 
 public class ZcKh_32 {
 
-    public static int longestValidParentheses(String s) {
-        int maxans = 0;
-        int[] dp = new int[s.length()];
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == ')') {
-                if (s.charAt(i-1) == '(') {
-                    dp[i] = (i>=2 ? dp[i-2]:0) + 2;
-                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
-                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
-                }
-                maxans = Math.max(maxans, dp[i]);
-            }
+  public static int longestValidParentheses(String s) {
+    int maxans = 0;
+    int[] dp = new int[s.length()];
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) == ')') {
+        if (s.charAt(i - 1) == '(') {
+          dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+        } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+          dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
         }
-        return maxans;
+        maxans = Math.max(maxans, dp[i]);
+      }
     }
+    return maxans;
+  }
 
-    public static void main(String[] args) {
-        String s = "(()";
-        int res = longestValidParentheses(s);
-        System.out.println(res);
-    }
+  public static void main(String[] args) {
+    String s = "(()";
+    int res = longestValidParentheses(s);
+    System.out.println(res);
+  }
 }
